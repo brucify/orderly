2c527088c9423f11572e73d296d46d7b4e205477
Bruce Yinhe <bruce.yinhe@gmail.com>
2022-05-14 20:51:59 +0200

    Use Vec instead of BTreeMap since depth already sorted

--- src/orderbook.rs
+++ src/orderbook.rs
@@ -2,7 +2,7 @@ use futures::channel::mpsc::{UnboundedReceiver, UnboundedSender};
 use rust_decimal::Decimal;
 use rust_decimal_macros::dec;
 use serde::{Deserialize, Serialize};
-use std::collections::BTreeMap;
+// use std::collections::BTreeMap;
 
 #[derive(Debug)]
 pub(crate) struct Tick {
@@ -21,7 +21,7 @@ pub(crate) enum Exchange {
     Binance,
 }
 
-#[derive(Debug, Deserialize, Serialize, PartialEq, Clone)]
+#[derive(Debug, Deserialize, Serialize, PartialEq, Eq, PartialOrd, Ord, Clone)]
 pub(crate) struct Bid {
     price: Decimal,
     amount: Decimal
@@ -33,7 +33,7 @@ impl Bid {
     }
 }
 
-#[derive(Debug, Deserialize, Serialize, PartialEq, Clone)]
+#[derive(Debug, Deserialize, Serialize, PartialEq, Eq, PartialOrd, Ord, Clone)]
 pub(crate) struct Ask {
     price: Decimal,
     amount: Decimal
@@ -62,42 +62,59 @@ impl MainOrderBook {
     /// Extracts the bids and asks from the `Tick`, then adds into its corresponding
     /// orderbook of the exchange.
     pub(crate) fn add(&mut self, t: Tick) {
-        let bids = t.bids.iter()
-            .map(|b| (b.price, b.amount))
-            .collect::<BTreeMap<Decimal, Decimal>>();
-        let asks = t.asks.iter()
-            .map(|b| (b.price, b.amount))
-            .collect::<BTreeMap<Decimal, Decimal>>();
+        // let bids = t.bids.iter()
+        //     .map(|b| (b.price, b.amount))
+        //     .collect::<BTreeMap<Decimal, Decimal>>();
+        // let asks = t.asks.iter()
+        //     .map(|b| (b.price, b.amount))
+        //     .collect::<BTreeMap<Decimal, Decimal>>();
 
         match t.exchange {
             Exchange::Bitstamp => {
-                self.bitstamp.bids = bids;
-                self.bitstamp.asks = asks;
+                self.bitstamp.bids = t.bids;
+                self.bitstamp.asks = t.asks;
             }
             Exchange::Binance => {
-                self.binance.bids = bids;
-                self.binance.asks = asks;
+                self.binance.bids = t.bids;
+                self.binance.asks = t.asks;
             }
         }
     }
 
     /// Returns a new `OrderBook` containing the merge bids and asks from both orderbooks.
     pub(crate) fn merged(&self) -> View {
-        let mut bids = self.bitstamp.bids.clone();
-        bids.merge(self.binance.bids.clone());
-        let mut asks = self.bitstamp.asks.clone();
-        asks.merge(self.binance.asks.clone());
+        // let mut bids = self.bitstamp.bids.clone();
+        // bids.merge(self.binance.bids.clone());
+        // let mut asks = self.bitstamp.asks.clone();
+        // asks.merge(self.binance.asks.clone());
+        //
+        // let bids = bids.iter()
+        //     .map(|(k, v)| Bid::new(k.clone(), v.clone()))
+        //     .rev()
+        //     .take(10)
+        //     .collect::<Vec<Bid>>();
+        //
+        // let asks = asks.iter()
+        //     .map(|(k, v)| Ask::new(k.clone(), v.clone()))
+        //     .take(10)
+        //     .collect::<Vec<Ask>>();
 
-        let bids = bids.iter()
-            .map(|(k, v)| Bid::new(k.clone(), v.clone()))
+        let mut bids: Vec<Bid> = self.bitstamp.bids.clone().into_iter()
+            .chain(self.binance.bids.clone())
+            .collect();
+        bids.sort_unstable();
+        let bids: Vec<Bid> = bids.into_iter()
             .rev()
             .take(10)
-            .collect::<Vec<Bid>>();
+            .collect();
 
-        let asks = asks.iter()
-            .map(|(k, v)| Ask::new(k.clone(), v.clone()))
+        let mut asks: Vec<Ask> = self.bitstamp.asks.clone().into_iter()
+            .chain(self.binance.asks.clone())
+            .collect();
+        asks.sort_unstable();
+        let asks: Vec<Ask> = asks.into_iter()
             .take(10)
-            .collect::<Vec<Ask>>();
+            .collect();
 
         let spread = match (bids.first(), asks.first()) {
             (Some(b), Some(a)) => a.price - b.price,
@@ -110,53 +127,57 @@ impl MainOrderBook {
 
 #[derive(Debug, PartialEq)]
 struct OrderBook {
-    pub(crate) bids: BTreeMap<Decimal, Decimal>,
-    pub(crate) asks: BTreeMap<Decimal, Decimal>,
+    // pub(crate) bids: BTreeMap<Decimal, Decimal>,
+    // pub(crate) asks: BTreeMap<Decimal, Decimal>,
+    pub(crate) bids: Vec<Bid>,
+    pub(crate) asks: Vec<Ask>,
 }
 
 impl OrderBook {
     fn new() -> OrderBook {
         OrderBook{
-            bids: BTreeMap::new(),
-            asks: BTreeMap::new(),
+            // bids: BTreeMap::new(),
+            // asks: BTreeMap::new(),
+            bids: vec![],
+            asks: vec![],
         }
     }
 }
 
-trait Merge {
-    fn merge(
-        &mut self,
-        other: BTreeMap<Decimal, Decimal>,
-    );
-
-    fn merge_and_keep(
-        &mut self,
-        other: BTreeMap<Decimal, Decimal>,
-        index: usize,
-    );
-}
-
-impl Merge for BTreeMap<Decimal, Decimal> {
-    /// Same as `BTreeMap::extend` but increments the value instead of replacing it
-    /// in case of a duplicate key.
-    fn merge(&mut self, other: BTreeMap<Decimal, Decimal>) {
-        other.into_iter().for_each(move |(k, v)| {
-            match self.get_mut(&k) {
-                None => { self.insert(k, v); },
-                Some(x) => *x += v, // increment instead of replace
-            }
-        });
-    }
-
-    /// Merges two `BTreeMap`. Returns everything before the given index.
-    fn merge_and_keep(&mut self, other: BTreeMap<Decimal, Decimal>, i: usize) {
-        self.merge(other);
-        if self.len() > i {
-            let key = self.keys().collect::<Vec<&Decimal>>()[i].clone();
-            self.split_off(&key);
-        }
-    }
-}
+// trait Merge {
+//     fn merge(
+//         &mut self,
+//         other: BTreeMap<Decimal, Decimal>,
+//     );
+//
+//     fn merge_and_keep(
+//         &mut self,
+//         other: BTreeMap<Decimal, Decimal>,
+//         index: usize,
+//     );
+// }
+//
+// impl Merge for BTreeMap<Decimal, Decimal> {
+//     /// Same as `BTreeMap::extend` but increments the value instead of replacing it
+//     /// in case of a duplicate key.
+//     fn merge(&mut self, other: BTreeMap<Decimal, Decimal>) {
+//         other.into_iter().for_each(move |(k, v)| {
+//             match self.get_mut(&k) {
+//                 None => { self.insert(k, v); },
+//                 Some(x) => *x += v, // increment instead of replace
+//             }
+//         });
+//     }
+//
+//     /// Merges two `BTreeMap`. Returns everything before the given index.
+//     fn merge_and_keep(&mut self, other: BTreeMap<Decimal, Decimal>, i: usize) {
+//         self.merge(other);
+//         if self.len() > i {
+//             let key = self.keys().collect::<Vec<&Decimal>>()[i].clone();
+//             self.split_off(&key);
+//         }
+//     }
+// }
 
 #[derive(Debug, PartialEq)]
 pub(crate) struct View {
@@ -173,7 +194,7 @@ pub(crate) fn channel() -> (UnboundedSender<Tick>, UnboundedReceiver<Tick>) {
 mod test {
     use crate::orderbook::*;
     use rust_decimal_macros::dec;
-    use std::collections::BTreeMap;
+    // use std::collections::BTreeMap;
 
     #[test]
     fn should_add_bitstamp_tick_to_empty() {
@@ -219,30 +240,54 @@ mod test {
          */
         assert_eq!(book, MainOrderBook {
             bitstamp: OrderBook{
-                bids: BTreeMap::from([
-                    (dec!(0.07358322), dec!(0.46500000)),
-                    (dec!(0.07357954), dec!(8.50000000)),
-                    (dec!(0.07357942), dec!(0.46500000)),
-                    (dec!(0.07357869), dec!(16.31857550)),
-                    (dec!(0.07357533), dec!(2.17483368)),
-                    (dec!(0.07354592), dec!(10.22442936)),
-                    (dec!(0.07354227), dec!(4.34696532)),
-                    (dec!(0.07352810), dec!(20.01159075)),
-                    (dec!(0.07350019), dec!(21.73733228)),
-                    (dec!(0.07348180), dec!(1.85000000)),
-                ]),
-                asks: BTreeMap::from([
-                    (dec!(0.07366569), dec!(0.46500000)),
-                    (dec!(0.07368584), dec!(16.30832712)),
-                    (dec!(0.07371456), dec!(2.17501178)),
-                    (dec!(0.07373077), dec!(4.35024244)),
-                    (dec!(0.07373618), dec!(8.50000000)),
-                    (dec!(0.07374400), dec!(1.85000000)),
-                    (dec!(0.07375536), dec!(11.31202728)),
-                    (dec!(0.07375625), dec!(6.96131361)),
-                    (dec!(0.07375736), dec!(0.00275804)),
-                    (dec!(0.07377938), dec!(0.00275807)),
-                ]),
+                // bids: BTreeMap::from([
+                //     (dec!(0.07358322), dec!(0.46500000)),
+                //     (dec!(0.07357954), dec!(8.50000000)),
+                //     (dec!(0.07357942), dec!(0.46500000)),
+                //     (dec!(0.07357869), dec!(16.31857550)),
+                //     (dec!(0.07357533), dec!(2.17483368)),
+                //     (dec!(0.07354592), dec!(10.22442936)),
+                //     (dec!(0.07354227), dec!(4.34696532)),
+                //     (dec!(0.07352810), dec!(20.01159075)),
+                //     (dec!(0.07350019), dec!(21.73733228)),
+                //     (dec!(0.07348180), dec!(1.85000000)),
+                // ]),
+                // asks: BTreeMap::from([
+                //     (dec!(0.07366569), dec!(0.46500000)),
+                //     (dec!(0.07368584), dec!(16.30832712)),
+                //     (dec!(0.07371456), dec!(2.17501178)),
+                //     (dec!(0.07373077), dec!(4.35024244)),
+                //     (dec!(0.07373618), dec!(8.50000000)),
+                //     (dec!(0.07374400), dec!(1.85000000)),
+                //     (dec!(0.07375536), dec!(11.31202728)),
+                //     (dec!(0.07375625), dec!(6.96131361)),
+                //     (dec!(0.07375736), dec!(0.00275804)),
+                //     (dec!(0.07377938), dec!(0.00275807)),
+                // ]),
+                bids: vec![
+                    Bid::new(dec!(0.07358322), dec!(0.46500000)),
+                    Bid::new(dec!(0.07357954), dec!(8.50000000)),
+                    Bid::new(dec!(0.07357942), dec!(0.46500000)),
+                    Bid::new(dec!(0.07357869), dec!(16.31857550)),
+                    Bid::new(dec!(0.07357533), dec!(2.17483368)),
+                    Bid::new(dec!(0.07354592), dec!(10.22442936)),
+                    Bid::new(dec!(0.07354227), dec!(4.34696532)),
+                    Bid::new(dec!(0.07352810), dec!(20.01159075)),
+                    Bid::new(dec!(0.07350019), dec!(21.73733228)),
+                    Bid::new(dec!(0.07348180), dec!(1.85000000)),
+                ],
+                asks: vec![
+                    Ask::new(dec!(0.07366569), dec!(0.46500000)),
+                    Ask::new(dec!(0.07368584), dec!(16.30832712)),
+                    Ask::new(dec!(0.07371456), dec!(2.17501178)),
+                    Ask::new(dec!(0.07373077), dec!(4.35024244)),
+                    Ask::new(dec!(0.07373618), dec!(8.50000000)),
+                    Ask::new(dec!(0.07374400), dec!(1.85000000)),
+                    Ask::new(dec!(0.07375536), dec!(11.31202728)),
+                    Ask::new(dec!(0.07375625), dec!(6.96131361)),
+                    Ask::new(dec!(0.07375736), dec!(0.00275804)),
+                    Ask::new(dec!(0.07377938), dec!(0.00275807)),
+                ],
             },
             binance: OrderBook::new(),
         });
